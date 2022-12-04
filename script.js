780089e885ff07e64ba47947b6764ddab4288ccb
Gavin Rockwell <gsrockwell910@gmail.com>
2022-12-03 21:11:54 -0500

    Add function to create a 16 by 16 grid and add drawing function

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-function createGrid(size) {
-    let sketchPad = document.querySelector(".sketchPad");
-    let squares = sketchPad.querySelectorAll("div");
-    squares.forEach((div) => div.remove());
-    sketchPad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+function createCanvas(size) {
+    let canvas = document.querySelector(".canvas");
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let numOfDivs = size * size;
-    for (let i = 0; i < numOfDivs; i++) {
+    let number = size * size;
+    for(let i = 0; i < number; i++) {
         let square = document.createElement("div");
-        square.addEventListener("mouseover", fillSquare);
-        square.style.backgroundColor = "white";
-        sketchPad.insertAdjacentElement("beforeend", square);
+        square.addEventListener("mouseover", colorSquares);
+        canvas.insertAdjacentElement("beforeend", square);
     }
-};
+}
 
-createGrid(16);
-
-function changeSize(input) {
-    let slider = document.querySelector(".slider");
-    let sliderOutput = document.querySelector(".sliderOutput");
-    slider.addEventListener("change", (e) => {
-        let value = e.target.value;
-        sliderOutput.textContent = `Grid Size: ${value} X ${value}`;
-    });
-    if (input >= 16 && input <= 100) {
-        createGrid(input);
-    } else {
-        console.log("ERROR!!!!!!!!!!!!");
-    }
-};
-
-Slider.onchange = (e) => changeSize(e.target.value)
-
-function fillSquare() {
+function colorSquares() {
     this.style.backgroundColor = "black";
 }
+
+createCanvas(16);
